a2cd2758e6afd966e99ff2d6d9ef3a566288d4d6
FranGamezRuiz <gamezruizf@gmail.com>
2020-06-04 12:20:38 +0200

    Implementación de los test

--- herramienta/test/tests_models.py
+++ herramienta/test/tests_models.py
@@ -9,6 +9,7 @@ class herramienta_test_class(TestCase):
         categoria = Categoria.objects.create(nombre="Jazztel", palabras_clave="jazztel")
         categoria1 = Categoria.objects.create(nombre="Movistar", palabras_clave="movistar")
         Tema.objects.create(nombre="ADSL", palabras_clave="adsl", categorias=[categoria, categoria1])
+        Tema.objects.create(nombre="Fibra", palabras_clave="Fibra optica", categorias=[categoria, categoria1])
         Tweet.objects.create(fecha_creado="2020-06-01", id_twitter="000000000001",texto="Prueba",truncado=False,geo=None,coordenadas=None,place=None,numero_ret=0,numero_fav=0,esFavorito=False,esRetweet=False,idioma="es",tema="ADSL",categoría="Jazztel",polaridad="Positivo",numero_Polaridad=1,busqueda="Actual",analisis="TextBlob")
         Tweet.objects.create(fecha_creado="2020-06-01", id_twitter="000000000002",texto="Prueba 2",truncado=False,geo=None,coordenadas=None,place=None,numero_ret=0,numero_fav=0,esFavorito=False,esRetweet=False,idioma="es",tema="ADSL",categoría="Jazztel",polaridad="Negativo",numero_Polaridad=-1,busqueda="Actual",analisis="TextBlob")
 
@@ -18,7 +19,7 @@ class herramienta_test_class(TestCase):
         categorias_tema = Tema.objects.get(pk=1).categorias
         categorias_nombre = []
         for categoria in categorias_tema:
-            categorias_nombre += categoria.nombre
+            categorias_nombre += [categoria.nombre]
         self.assertTrue(nombre_categoria in categorias_nombre)
 
 
